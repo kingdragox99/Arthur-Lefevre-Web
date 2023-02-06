de07103c86fc1f69de6f2c4757d506a022dbb2ec
Arthur Lefevre <kingdragox99@gmail.com>
2023-02-06 20:18:47 +0100

    feat: add Experience

--- pages/index.vue
+++ pages/index.vue
@@ -60,25 +60,48 @@ main.px-4.py-4.font-inco
   section#experience.h-auto.w-full.mb-8
     h1.font-noto.font-bold.text-xl.text-primary.mb-4 Experience
     .flex.w-full.py-2.gap-2
-      a.btn.btn-sm(href='#slide1') Becode
-      a.btn.btn-sm(href='#slide2') Altab
+      a.btn.btn-sm(href='#slide1') Altab
+      a.btn.btn-sm(href='#slide2') Becode
     .carousel.w-full
       #slide1.carousel-item.relative.w-full.flex.flex-col
+        p.font-inco.text-primary.font-bold.text-lg
+          | Internship
+          |
+          span.font-noto.font-bold.text-secondary @ Altab
+        p.font-inco.text-sm.mb-4 December 2022 - February 2023
+        ul
+          li.flex.mb-4
+            Icon.text-primary(name='ic:baseline-arrow-right' size='1.5rem')
+            p.w-4of5 
+              | I did a 3 months internship at
+              |
+              a.text-primary.underline.linkhover(href='https://www.altab.be/') Altab
+              |
+              | it's a web development company, during this internship I did the front-end integration of the new website of
+              |
+              a.text-primary.underline.linkhover(href='https://www.verdin.be/') Verdin.be
+          li.flex.mb-4
+            Icon.text-primary(name='ic:baseline-arrow-right' size='1.5rem')
+            p.w-4of5 I learned to use Nuxt.js, i18n, Vuelidate, Pugjs
+
+      #slide2.carousel-item.relative.w-full.flex.flex-col
         p.font-inco.text-primary.font-bold.text-lg
           | Internship
           |
           span.font-noto.font-bold.text-secondary @ Becode.org
         p.font-inco.text-sm.mb-4 May 2022 - December 2022
         ul
-          li.flex.items-center
+          li.flex.mb-4
             Icon.text-primary(name='ic:baseline-arrow-right' size='1.5rem')
-          li.flex.items-center
+            p.w-4of5 
+              | I followed the web development training of
+              |
+              a.text-primary.underline.linkhover(href='https://becode.org/') Becode.org
+              | , this training lasts 7 months and its goal is to teach us the basics of web development, with a learning pedagogue called "Active Methodology": learn by doing
+          li.flex.mb-4
             Icon.text-primary(name='ic:baseline-arrow-right' size='1.5rem')
-      #slide2.carousel-item.relative.w-full.h-64
-        p.font-inco.text-primary.font-bold.text-lg
-          | Internship
-          |
-          span.font-noto.font-bold.text-secondary @ Altab.be
+            p.w-4of5 I learned to use HTML, CSS, Javascript, Vue.js, Node.js, MongoDB, Tailwind
+
   section#projects.h-auto.w-full.mb-8
     h1.font-noto.font-bold.text-xl.text-primary.mb-4 Projects
   section#contact.h-auto.w-full.mb-8
