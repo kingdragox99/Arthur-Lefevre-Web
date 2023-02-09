3aa219a15f2da1b0a362642e14ac083244731dd4
Arthur Lefevre <kingdragox99@gmail.com>
2023-02-09 04:20:26 +0100

    refactor: change project change nav bar

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -1,42 +1,53 @@
 <template lang="pug">
-header.font-inco.bg-other.flex.flex-col.items-center.px-4.py-4.md_text-xl
-  .flex.justify-between.items-center.mb-3.w-full.lg_w-1of2.xl_w-1of3
-    label.swap.swap-rotate.w-14.hover_text-primary.md_w-28(for='mode')
-      input#mode(type='checkbox' @click='toggleTheme' aria-label='Change mode')
-      IconMoon.swap-off.w-6
-      IconSun.swap-on.w-6
-    Logo.w-1of2.fill-logo.md_w-1of2
-    .dropdown.dropdown-end.w-14.md_w-28
-      label.btn.border-none.flex.justify-center.items-center.hover_text-primary(
-        tabindex='0'
-      )
-        Icon(name='majesticons:translate' size='1.5rem')
-      ul.dropdown-content.menu.p-2.shadow.bg-neutral.rounded-box.w-52(
-        tabindex='0'
-      )
-        li.hover_bg-background
-          NuxtLink(to='/') English
-        li.hover_bg-background
-          NuxtLink(to='/fr') Français
-  nav.w-full.md_w-auto
-    ul.flex.justify-around
-      li
-        a.hover_text-primary.m-2(href='#about' aria-label='go to about') {{ $t('pages.header.about') }}
-      li
-        a.hover_text-primary.m-2(
-          href='#experience'
-          aria-label='go to experience'
-        ) {{ $t('pages.header.experience') }}
-      li
-        a.hover_text-primary.m-2(href='#projects' aria-label='go to projects') {{ $t('pages.header.projects') }}
-      li
-        a.hover_text-primary.m-2(href='#contact' aria-label='go to contact') {{ $t('pages.header.contact') }}
+.drawer.drawer-end
+  input#drawer.drawer-toggle(type='checkbox')
+
+  .drawer-content
+    header.px-4.py-4.bg-other.grid.grid-cols-10.mb-3.w-full.place-items-center.bg-other
+      label.btn.btn-circle.drawer-button(for='drawer')
+        Menu
+      Logo.w-full.fill-logo.col-start-5.col-end-7
+  .drawer-side
+    label.drawer-overlay(for='drawer')
+    .bg-other.w-1of5.shadow-lg.mt-10
+      nav
+        ul.text-center
+          li.mb-4.text-xl.bg-neutral.p-2.m-2.rounded-3xl.hover_bg-background
+            a.m-2(href='#about' aria-label='go to about') {{ $t('pages.header.about') }}
+          li.mb-4.text-xl.bg-neutral.p-2.m-2.rounded-3xl.hover_bg-background
+            a.m-2(href='#experience' aria-label='go to experience') {{ $t('pages.header.experience') }}
+          li.mb-4.text-xl.bg-neutral.p-2.m-2.rounded-3xl.hover_bg-background
+            a.m-2(href='#projects' aria-label='go to projects') {{ $t('pages.header.projects') }}
+          li.mb-4.text-xl.bg-neutral.p-2.m-2.rounded-3xl.hover_bg-background
+            a.m-2(href='#contact' aria-label='go to contact') {{ $t('pages.header.contact') }}
+      .flex.justify-center
+        .dropdown.dropdown-end
+          label.btn.border-none.flex.justify-center.items-center.hover_text-primary(
+            tabindex='0'
+          )
+            Icon(name='majesticons:translate' size='1.5rem')
+          ul.dropdown-content.menu.p-2.shadow.bg-neutral.rounded-box.w-52(
+            tabindex='0'
+          )
+            li.hover_bg-background
+              NuxtLink(to='/') English
+            li.hover_bg-background
+              NuxtLink(to='/fr') Français
+        label.swap.swap-rotate.w-14.hover_text-primary(for='mode')
+          input#mode(
+            type='checkbox'
+            @click='toggleTheme'
+            aria-label='Change mode'
+          )
+          IconMoon.swap-off.w-6
+          IconSun.swap-on.w-6
 </template>
 
 <script setup>
 import Logo from '@@/assets/svg/logo-w.svg';
 import IconSun from '@/assets/icons/sun.svg';
 import IconMoon from '@/assets/icons/moon.svg';
+import Menu from '@/assets/icons/menu.svg';
 
 const colorMode = useColorMode();
 
